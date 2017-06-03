24fa24d16c1746b041d034b8f6fd59afaa61ed9a
Donald Pierce <thedonaldjpierce@gmail.com>
2017-06-03 13:37:52 -0400

    initialize repository

--- biniterations.c
+++ biniterations.c
@@ -1,22 +1,22 @@
 {
 	#include <vector>
 	
-	TFile * 2015 = TFile::Open("ZeroBias2015.p2634.PeriodJ.root");
+	TFile *file = TFile::Open("ZeroBias2016new.13Runs.root");
 
 	Float_t setalg;
 	TBranch *b_setalg = new TBranch();
-	tree->SetBranchAddress("settopoclps", &setalg, &b_setalg);
+	tree->SetBranchAddress("settopocl", &setalg, &b_setalg);
 
 	Float_t metalg;
 	TBranch *b_metalg = new TBranch();
-	tree->SetBranchAddress("mettopoclps", &metalg, &b_metalg);
+	tree->SetBranchAddress("mettopocl", &metalg, &b_metalg);
 
-	int tail = 121;
+	int tail = 1;
 
 	//==================================================================================================================================================//
-	//Initialize scatter plot of alg met vs. sqrt alg set ***NOTE: CHANGE RANGE FOR SIGNAL EVENTS***	
+	//Initialize scatter plot of alg met vs. sqrt alg set 
+	//***NOTE: CHANGE RANGE FOR SIGNAL EVENTS***	
 	TH1F *algset = new TH1F("algset", "sqrt of Algorithm SET", 100, 0., 100.);
-	TH1F *algmet = new TH1F("algmet", "Algorith MET", 200, 0., 200.);
 
 	bool pass;
 	Long64_t nentries = tree->GetEntries();
@@ -25,10 +25,14 @@
 		tree->GetEntry(i);
 
 		// Only pass the events that are in the specified tail of the algorithm
-		if (metalg > tail)
+		if ("passrndm" > 0.1)
 		{
-			algset->Fill(sqrt(setalg));
+			if (metalg > tail)
+			{
+				algset->Fill(sqrt(setalg));
+			}
 		}
+		
 	}
 
 	//Get the bin content of each bin of sq.rt(SET)
@@ -43,15 +47,15 @@
 		if (binpop != 0)
 		{
 			binnum[j] = algset->GetBin(j);
+			cout << binnum[j];
 		}
 		else
 		{
 			binnum[j] = 0;
 		}
 	}
-
-	//Count the non-zero elements of binnum for the number of histograms we will generate
 	
+	//Count the non-zero elements of binnum for the number of histograms we will generate
 	int counter = 0;
 	for (int k = 1; k < 100; k++)
 	{
@@ -85,42 +89,57 @@
 	}
 
 	// Parse through the tree again, and fill each MET histogram only with events in their respective sqrt(set) bins
-	Long64_t nentries = tree->GetEntries();
+	
 	for (int i = 0; i < nentries; i++)
 	{
-		tree->GetEntry(i);		
-		for (int p = 1; p < nhist; p++)
+		if ("passrndm" > 0.1)
 		{
+			tree->GetEntry(i);
+			for (int p = 1; p < nhist; p++)
+			{
 				if (binarray[p] < sqrt(setalg) && sqrt(setalg) < binarray[p] + 1)
 				{
 					myhist[p]->Fill(metalg);
 				}
-			
+			}
 		}
 	}
 
-	// Fit each histgram to a rayleigh distribution and save their sigmas to an array
+	for (int n = 1; n < nhist; n++)
+	{
+		//myhist[n]->SetTitle(metalg "for bin %d of sq rt SET");
+		myhist[n]->GetYaxis()->SetTitle("Number of Events");
+		myhist[n]->GetXaxis()->SetTitle("MET [GeV]");
+	}
+
 	// Initialize the Rayleigh Distribution
-	TF1 *raydist = new TF1("raydist", "[0]*(1/[1])*(x/[1])*exp(-.5*(x/[1])*(x/[1]))");
-	raydist->SetParameters(0, 100000.);
-	raydist->SetParameters(1, 1.);
-	raydist->SetParLimits(0, 0.1, 10000000.);
-	raydist->SetParLimits(1, 0.1, 10000000.);
+	TF1 *func = new TF1("func", "[0]*(1/[1])*(x/[1])*exp(-.5*(x/[1])*(x/[1]))");
+	func->SetParameters(0, 100000.);
+	func->SetParameters(1, 1.);
+	func->SetParLimits(0, 0.1, 10000000.);
+	func->SetParLimits(1, 0.1, 10000000.);
 
-
-	//TString a = "bin";
-	//TString b = ".png";
+	// Plot events vs. MET/SIGMA	
+	double sigmaarray[nhist];
 	TCanvas *mycanv[nhist];
 	char *canvname = new char[nhist];
 	for (int m = 1; m < nhist; m++)
 	{
 		sprintf(canvname, "canv%d", m);
 		mycanv[m] = new TCanvas(canvname, "");
-		myhist[m]->Fit("raydist", "L");
-		//TString filename = a + binarray[m] + b;
-		myhist[m]->Draw();
+		mycanv[m]->SetLogy();
+		myhist[m]->Fit("func", "L");
+		sigmaarray[m] = func->GetParameter(1);
+		mycanv[m]->Print(Form("../Pictures/%s.png", myhist[m]->GetName()));
 	}
 
-
-
+	ofstream sigmafile;
+	sigmafile.open("sigmaarray.txt");
+	sigmafile << "sigma" << "\n";
+	for (int p = 1; p < nhist; p++)
+	{
+		sigmafile << sigmaarray[p] << "\n";
+	}
+	sigmafile.close();
+	
 }
